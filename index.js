5f2188314fe01719733721593730f77650e9ef1b
Guilherme Monni <gmonni20@gmail.com>
2026-03-01 17:55:44 -0300

    fix server

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ import pino from 'pino'
 const logger = pino()
 const server = fastify({ logger:true })
 
-server.get('/', () => {
-    return logger.info('Servidor rodando.')
+server.get('/test', () => {
+    logger.info('Servidor rodando.')
+    return { message: "Servidor on." }
+})
+
+server.listen({ port: 8000, host: '0.0.0.0' }, (err) => {
+    if (err) {
+        logger.error(err)
+        process.exit(1)
+    }
 })
